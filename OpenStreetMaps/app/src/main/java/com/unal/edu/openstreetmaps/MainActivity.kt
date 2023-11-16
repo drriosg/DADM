cb6c917684343dd95b581e515e405a4c47f2fb17
David Rios <drriosg@unal.edu.co>
2023-11-15 19:25:13 -0500

    Reto9

--- OpenStreetMaps/app/src/main/java/com/unal/edu/openstreetmaps/MainActivity.kt
+++ OpenStreetMaps/app/src/main/java/com/unal/edu/openstreetmaps/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.unal.edu.openstreetmaps
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback {
-    private var mGoogleMap: GoogleMap? = null
+class MainActivity : AppCompatActivity(), OnMapReadyCallback,
+    GoogleMap.OnMyLocationButtonClickListener {
+    private lateinit var mGoogleMap: GoogleMap
+
+    companion object{
+        const val REQUEST_CODE_LOCATION = 0
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,5 +28,66 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onMapReady(googleMap: GoogleMap) {
         mGoogleMap = googleMap
+        mGoogleMap.setOnMyLocationButtonClickListener(this)
+        enableLocation()
+    }
+
+    private fun isLocationPermissionGranted() = ContextCompat.checkSelfPermission(
+        this,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
+
+    private fun enableLocation(){
+        if(!::mGoogleMap.isInitialized) return
+        if(isLocationPermissionGranted()){
+            mGoogleMap.isMyLocationEnabled = true
+        } else {
+            requestLocationPermission()
+        }
+    }
+
+    private fun requestLocationPermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        ){
+            Toast.makeText(this, "Ve a ajustes y acepta los permios", Toast.LENGTH_SHORT).show()
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_CODE_LOCATION
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode){
+            REQUEST_CODE_LOCATION -> if (grantResults.isNotEmpty() && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                mGoogleMap.isMyLocationEnabled = true
+            } else {
+                Toast.makeText(this, "Para activar la localizacion ve a los ajustes y acepta los permisos", Toast.LENGTH_SHORT).show()
+            }
+            else -> {}
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onResumeFragments() {
+        super.onResumeFragments()
+        if(!::mGoogleMap.isInitialized) return
+        if(isLocationPermissionGranted()){
+            mGoogleMap.isMyLocationEnabled = false
+            Toast.makeText(this, "Para activar la localización ve a ajustes y acepta los permisos", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onMyLocationButtonClick(): Boolean {
+        return false
     }
 }
